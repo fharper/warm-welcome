957c5a116a8178171630d0e140fb65d61386be9f
Frédéric Harper <hi@fred.dev>
2023-08-14 17:28:39 -0400

    fix linting issues

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function titleCase(str) {
     str = str.toLowerCase();
     str = str.split(' ');
 
-    for (var i = 0; i < str.length; i++) {
-      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    for (let i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
     }
 
     return str.join(' ');
